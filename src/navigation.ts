25c36a3aacd5cf79d7951f571f00ad20eb2e54cc
waynelan828 <waynelan828@gmail.com>
2025-09-17 17:30:19 +0800

    #	modified:   src/components/widgets/Footer.astro #	modified:   src/navigation.ts

--- src/navigation.ts
+++ src/navigation.ts
@@ -6,7 +6,7 @@ export const headerData = {
     { text: '關於我們', href: getPermalink('/about') },
     { text: '服務項目', href: getPermalink('/services') },
     { text: '專案案例', href: getPermalink('/portfolio') },
-    { text: 'BLOG/木工新知', href: getBlogPermalink() },
+    { text: 'BLOG', href: getBlogPermalink() },
     { text: '聯絡我們', href: getPermalink('/contact') },
   ],
   actions: [],
@@ -33,7 +33,7 @@ export const footerData = {
         { text: '關於我們', href: getPermalink('/about') },
         { text: '服務項目', href: getPermalink('/services') },
         { text: '專案案例', href: getPermalink('/portfolio') },
-        { text: 'BLOG/木工新知', href: getBlogPermalink() },
+        { text: 'BLOG', href: getBlogPermalink() },
         { text: '聯絡我們', href: getPermalink('/contact') },
       ],
     },
@@ -41,7 +41,7 @@ export const footerData = {
       title: '聯絡資訊',
       links: [
         { text: 'Email: info@nice8.works', href: 'mailto:info@nice8.works' },
-        { text: '地址: 235新北市中和區板南路520巷5號', href: '#' },
+        { text: '地址: 新北市中和區板南路520巷5號', href: '#' },
         { text: '營業時間: 週一至週五 08:00-17:00', href: '#' },
       ],
     },
@@ -50,18 +50,18 @@ export const footerData = {
     { text: '條款', href: getPermalink('/terms') },
     { text: '隱私權政策', href: getPermalink('/privacy') },
   ],
-  socialLinks: [
-    {
-      ariaLabel: 'Facebook',
-      icon: 'tabler:brand-facebook',
-      href: 'https://facebook.com/nice8works',
-    },
-    {
-      ariaLabel: 'Instagram',
-      icon: 'tabler:brand-instagram',
-      href: 'https://instagram.com/nice8works',
-    },
-  ],
+  //socialLinks: [
+  //  {
+  //    ariaLabel: 'Facebook',
+  //    icon: 'tabler:brand-facebook',
+  //    href: 'https://facebook.com/nice8works',
+  //  },
+  //  {
+  //    ariaLabel: 'Instagram',
+  //    icon: 'tabler:brand-instagram',
+  //    href: 'https://instagram.com/nice8works',
+  //  },
+  //],
 
   footNote: `
     © 大鋒室內裝修工程有限公司 · All rights reserved.
